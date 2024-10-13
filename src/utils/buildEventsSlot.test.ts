6f66e7f24aee75faf19722b4faa35c5659132d23
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-13 14:43:48 +0200

    Implement buildEventSlot : n events overlap + improve codes readibility

--- src/utils/buildEventsSlot.test.ts
+++ src/utils/buildEventsSlot.test.ts
@@ -5,64 +5,103 @@ import {
   ScreenDimension,
 } from "./buildEventsSlot";
 
-describe("buildEventsSlotWithPosition", () => {
-  const useCases: Array<{
-    description: string;
-    events: EventsSlot;
-    screenDimension: ScreenDimension;
-    expectedResult: EventsSlotToDisplay;
-    pixelsForEachMinute: number;
-    calendarStartTime: string;
-  }> = [
-    {
-      description:
-        "Should generate n events no overlap take full width of available zone. e.g :3 events ",
-      events: [
-        {
-          id: 1,
-          start: "9:00",
-          duration: 60,
-        },
-        {
-          id: 2,
-          start: "10:00",
-          duration: 60,
-        },
-        {
-          id: 3,
-          start: "11:15",
-          duration: 50,
-        },
-      ],
-      pixelsForEachMinute: 1.25,
-      calendarStartTime: "9:00",
-      screenDimension: { height: 900, width: 1400 },
-      expectedResult: [
-        { id: 1, top: 0, left: 0, width: 1400, height: 75 },
-        { id: 2, top: 75, left: 0, width: 1400, height: 75 },
-        { id: 3, top: 168.75, left: 0, width: 1400, height: 62.5 },
-      ],
-    },
-  ];
+describe("buildEventSlot", () => {
+  describe("Utility builder: generateEventsWithTimeInMinutes", () => {});
+  describe("Utility builder: findOverlappingEvents", () => {});
+  describe("buildEventsSlotWithPosition", () => {
+    const useCases: Array<{
+      description: string;
+      events: EventsSlot;
+      screenDimension: ScreenDimension;
+      expectedResult: EventsSlotToDisplay;
+      pixelsForEachMinute: number;
+      calendarStartTime: string;
+    }> = [
+      {
+        description:
+          "Should generate n events no overlap take full width of available zone. e.g :3 events ",
+        events: [
+          {
+            id: 1,
+            start: "9:00",
+            duration: 60,
+          },
+          {
+            id: 2,
+            start: "10:00",
+            duration: 60,
+          },
+          {
+            id: 3,
+            start: "11:15",
+            duration: 50,
+          },
+        ],
+        pixelsForEachMinute: 1.25,
+        calendarStartTime: "9:00",
+        screenDimension: { height: 900, width: 1400 },
+        expectedResult: [
+          { id: 1, top: 0, left: 0, width: 1400, height: 75 },
+          { id: 2, top: 75, left: 0, width: 1400, height: 75 },
+          { id: 3, top: 168.75, left: 0, width: 1400, height: 62.5 },
+        ],
+      },
+      {
+        description:
+          "Should generate n events overlap happens in the same time frames. e.g : 3 events overlaps ",
+        events: [
+          {
+            id: 1,
+            start: "9:00",
+            duration: 60,
+          },
+          {
+            id: 2,
+            start: "9:15",
+            duration: 60,
+          },
+          {
+            id: 3,
+            start: "9:30",
+            duration: 50,
+          },
+        ],
+        pixelsForEachMinute: 1.25,
+        calendarStartTime: "9:00",
+        screenDimension: { height: 900, width: 1400 },
+        expectedResult: [
+          { id: 1, top: 0, left: 0, width: 1400 / 3, height: 75 },
+          { id: 2, top: 18.75, left: 1400 / 3, width: 1400 / 3, height: 75 },
+          {
+            id: 3,
+            top: 37.5,
+            left: (1400 / 3) * 2,
+            width: 1400 / 3,
+            height: 62.5,
+          },
+        ],
+      },
+    ];
 
-  useCases.forEach(
-    ({
-      description,
-      events,
-      screenDimension,
-      pixelsForEachMinute,
-      expectedResult,
-      calendarStartTime,
-    }) => {
-      test(description, () => {
-        const result = buildEventsSlotWithPosition(
-          events,
-          screenDimension,
-          pixelsForEachMinute,
-          calendarStartTime
-        );
-        expect(result).toEqual(expectedResult);
-      });
-    }
-  );
+    useCases.forEach(
+      ({
+        description,
+        events,
+        screenDimension,
+        pixelsForEachMinute,
+        expectedResult,
+        calendarStartTime,
+      }) => {
+        test(description, () => {
+          const result = buildEventsSlotWithPosition(
+            events,
+            screenDimension,
+            pixelsForEachMinute,
+            calendarStartTime
+          );
+          expect(result).toEqual(expectedResult);
+        });
+      }
+    );
+  });
 });
--- src/utils/buildEventsSlot.ts
+++ src/utils/buildEventsSlot.ts
@@ -7,12 +7,13 @@ type EventSlot = {
   duration: number;
 };
 export type EventsSlot = EventSlot[];
-
+type EventOverlapsRecord = Map<number, number[]>;
 export type ScreenDimension = {
   height: number;
   width: number;
 };
-
+type PixelPerMinute = number;
+type CalendarStarTime = string;
 export type EventsSlotToDisplay = {
   id: number;
   top: number;
@@ -21,32 +22,107 @@ export type EventsSlotToDisplay = {
   height: number;
 }[];
 
-export const buildEventsSlotWithPosition = (
-  events: EventsSlot,
-  screenDimension: ScreenDimension,
-  pixelsForEachMinute: number,
-  calendarStartTime: string
-): EventsSlotToDisplay => {
-  const { width: screenWidth } = screenDimension;
-  const eventWidth = screenWidth;
-  const eventHorizontalPosition = 0;
+type EventSlotWithPosition = {
+  id: number;
+  eventStartingVerticalPosition: number;
+  eventEndingVerticalPosition: number;
+  eventHeight: number;
+};
+
+export const convertEventsTimeToMinute = (
+  event: EventSlot,
+  calendarStartTime: CalendarStarTime,
+  pixelsForEachMinute: PixelPerMinute
+): EventSlotWithPosition => {
+  const eventStartTimeInMinutes = timeToMinutes(event.start);
 
   const calendarStartTimeInMinutes = timeToMinutes(calendarStartTime);
 
-  const eventSlots = events.map((event) => {
-    const eventStarTimeInMinutes = timeToMinutes(event.start);
-    const eventVerticalPosition =
-      (eventStarTimeInMinutes - calendarStartTimeInMinutes) *
-      pixelsForEachMinute;
-    const eventHeight = event.duration * pixelsForEachMinute;
+  const eventHeight = event.duration * pixelsForEachMinute;
 
-    return {
+  //eventStartingVerticalPosition and eventEndingVerticalPosition as the distance from event to the top which is the beginning of a day. e:g 9:00 is the beginning
+  const eventStartingVerticalPosition =
+    (eventStartTimeInMinutes - calendarStartTimeInMinutes) *
+    pixelsForEachMinute;
+
+  const eventEndingVerticalPosition =
+    eventStartingVerticalPosition + eventHeight;
+
+  return {
+    id: event.id,
+    eventStartingVerticalPosition,
+    eventEndingVerticalPosition,
+    eventHeight,
+  };
+};
+const findOverlappingEvents = (
+  sortedEvents: EventSlotWithPosition[]
+): EventOverlapsRecord => {
+  const eventOverlapsRecord: EventOverlapsRecord = new Map();
+
+  for (let i = 0; i < sortedEvents.length; i++) {
+    const event = sortedEvents[i];
+
+    for (let j = i + 1; j < sortedEvents.length; j++) {
+      const otherEvent = sortedEvents[j];
+
+      if (
+        otherEvent.eventStartingVerticalPosition <
+        event.eventEndingVerticalPosition
+      ) {
+        if (!eventOverlapsRecord.has(i)) {
+          eventOverlapsRecord.set(i, []);
+        }
+        if (!eventOverlapsRecord.has(j)) {
+          eventOverlapsRecord.set(j, []);
+        }
+        eventOverlapsRecord.get(i)!.push(j);
+        eventOverlapsRecord.get(j)!.push(i);
+      }
+    }
+  }
+  return eventOverlapsRecord;
+};
+
+export const buildEventsSlotWithPosition = (
+  events: EventsSlot,
+  screenDimension: ScreenDimension,
+  pixelsForEachMinute: PixelPerMinute,
+  calendarStartTime: string
+): EventsSlotToDisplay => {
+  const { width: screenWidth } = screenDimension;
+  const eventsWithTimeToMinute = events.map((event) =>
+    convertEventsTimeToMinute(event, calendarStartTime, pixelsForEachMinute)
+  );
+
+  // Sort events by start time : earlier starting time will have lower distance to the top
+  const sortedEvents = [...eventsWithTimeToMinute].sort(
+    (a, b) => a.eventStartingVerticalPosition - b.eventStartingVerticalPosition
+  );
+
+  const eventSlots: EventsSlotToDisplay = [];
+  const eventOverlapsRecord = findOverlappingEvents(sortedEvents);
+
+  for (let i = 0; i < sortedEvents.length; i++) {
+    const event = sortedEvents[i];
+
+    const overlappingEvents = eventOverlapsRecord.get(i) || [];
+    const groupOverlapEvents = [i, ...overlappingEvents].sort();
+    const totalOverlaps =
+      groupOverlapEvents.length > 1 ? groupOverlapEvents.length : 1;
+    const eventWidth = screenWidth / totalOverlaps;
+
+    const eventPositionInGroup = groupOverlapEvents.indexOf(i);
+    const eventLeft = eventPositionInGroup * eventWidth;
+
+    eventSlots.push({
       id: event.id,
-      top: eventVerticalPosition,
-      left: eventHorizontalPosition,
+      top: event.eventStartingVerticalPosition,
+      left: eventLeft,
       width: eventWidth,
-      height: eventHeight,
-    };
-  });
+      height: event.eventHeight,
+    });
+  }
+
   return eventSlots;
 };
