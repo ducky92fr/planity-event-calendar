d3b52be987df24930a8076a736d637fdb3a66d94
Duc NGUYEN <nguyentduc92@gmail.com>
2024-10-13 12:57:47 +0200

    Implement buildEventSlot : n events no overlap (cover also the one event test)

--- src/utils/buildEventsSlot.test.ts
+++ src/utils/buildEventsSlot.test.ts
@@ -16,18 +16,32 @@ describe("buildEventsSlotWithPosition", () => {
   }> = [
     {
       description:
-        "Should generate 1 event no overlap take full width of available zone",
+        "Should generate n events no overlap take full width of available zone. e.g :3 events ",
       events: [
         {
           id: 1,
           start: "9:00",
           duration: 60,
         },
+        {
+          id: 2,
+          start: "10:00",
+          duration: 60,
+        },
+        {
+          id: 3,
+          start: "11:15",
+          duration: 50,
+        },
       ],
       pixelsForEachMinute: 1.25,
       calendarStartTime: "9:00",
       screenDimension: { height: 900, width: 1400 },
-      expectedResult: [{ id: 1, top: 0, left: 0, width: 1400, height: 75 }],
+      expectedResult: [
+        { id: 1, top: 0, left: 0, width: 1400, height: 75 },
+        { id: 2, top: 75, left: 0, width: 1400, height: 75 },
+        { id: 3, top: 168.75, left: 0, width: 1400, height: 62.5 },
+      ],
     },
   ];
 
